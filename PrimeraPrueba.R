6f2b576a918ccc67b1ceb8fe70d843254b9fa272
Emilio Hache <spam.emilio@gmail.com>
2021-06-30 00:53:30 -0300

    Prestaciones AUH y Gráfico

--- PrimeraPrueba.R
+++ PrimeraPrueba.R
@@ -1,6 +1,6 @@
 setwd("~/Diplomatura/Trabajo Final/Grupo4_Final")
 library(tidyverse)
-options()
+options(scipen = 100)
 
 prestaciones_previsionales_sipa_por_tipo <- read_csv("https://infra.datos.gob.ar/catalog/sspm/dataset/189/distribution/189.1/download/prestaciones-previsionales-sipa-por-tipo.csv")
 
@@ -15,4 +15,15 @@ ggplot(aes(x=indice_tiempo, y = cantidad)) +
 
 totales_por_prestacion <- prestaciones_tidy %>% 
   group_by(prestacion) %>% 
-  summarise(total = sum(cantidad, na.rm = TRUE))
+  summarise(total = max(cantidad, na.rm = TRUE))
+
+prestaciones_auh <- prestaciones_tidy %>% 
+  filter(str_detect(prestacion, "asignacion_universal"))
+
+prestaciones_auh %>% 
+  ggplot(aes(x=indice_tiempo, y = cantidad)) +
+  geom_line(aes(color=prestacion))
+
+totales_por_tipo_auh <- prestaciones_auh %>% 
+  group_by(prestacion) %>% 
+  summarise(total = max(cantidad, na.rm = TRUE))
